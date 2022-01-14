4de3fc7b9e2d37604c87a31be305937288209310
Josue Lopez <josue.ismael.lopez.ramirez@gmail.com>
2022-01-14 17:52:53 -0600

    v2

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = {
+    origin: '*',
+    'Access-Control-Allow-Origin': '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(options);
+
   await app.listen(parseInt(process.env.PORT, 10));
 }
 bootstrap();
